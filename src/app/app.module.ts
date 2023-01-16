4adea8d1641b2ce4cf723ddf88d94e15cd627bd3
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-01-16 10:35:15 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { DatePickerComponent } from '../date-picker/date-picker.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,ReactiveFormsModule, MatDatepickerModule,MatFormFieldModule, MatInputModule],
-  declarations: [ AppComponent, HelloComponent, DatePickerComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+  
+  ],
+  declarations: [AppComponent, DatePickerComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
