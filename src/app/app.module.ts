53bca0c680caccfda3e9b94c9107b49bd4fca5e3
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-01-16 11:05:20 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -11,6 +13,7 @@ import { DatePickerComponent } from '../date-picker/date-picker.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     MatDatepickerModule,
