ef5272e5fd5c99b03e4c4ff5afbfff00ed199220
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-01-16 12:22:27 -0600

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, VERSION } from '@angular/core';
+import { DatePickerComponent } from '../date-picker/date-picker.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  standalone:true,
+  imports:[DatePickerComponent]
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
--- src/date-picker/date-picker.component.ts
+++ src/date-picker/date-picker.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css']
+  styleUrls: ['./date-picker.component.css'],
+  standalone:true,
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule
+
+  ]
+
 })
 export class DatePickerComponent implements OnInit {
 
