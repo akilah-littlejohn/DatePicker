b9cfcae85b233511d951780c492e619aedd967df
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-01-17 10:00:58 -0600

    4 files modified

--- src/date-picker/date-picker.component.ts
+++ src/date-picker/date-picker.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {  FormsModule, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css']
+  styleUrls: ['./date-picker.component.css'],
 })
 export class DatePickerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
   selectedDate = new FormControl(new Date());
-
-
- 
-  ngOnInit() {
+  
+  ngOnInit() {}
+  log(value) {
+    console.log(value);
   }
-
 }
