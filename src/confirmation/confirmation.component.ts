647363a815716ea6e186e45de639fe9e9d947d5c
akilah-littlejohn <akilahlittlejohn@gmail.com>
2023-01-18 06:45:53 -0600

    1 file modified

--- src/confirmation/confirmation.component.ts
+++ src/confirmation/confirmation.component.ts
@@ -3,16 +3,15 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css']
+  styleUrls: ['./confirmation.component.css'],
 })
 export class ConfirmationComponent implements OnInit {
   @Input() confirmed_date: Date;
   @Input() confirmed_serviceType: string;
-  @Input() confirmed_specialRequest:string;
-  @Input() resubmit:boolean
-  constructor() { }
+  @Input() confirmed_specialRequest: string;
+  @Input() resubmit: boolean;
+  
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
